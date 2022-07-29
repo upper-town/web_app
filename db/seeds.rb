116a80b66316b19c7a0dce21a4aecc2aeebfcc49
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2022-07-28 21:47:14 -0300

    Create servers in db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+require "factory_bot_rails"
+
+class SeederAll
+  def run
+    return if Rails.env.test?
+
+    Rails.logger.info "Seeding data for all environments"
+  end
+end
+
+class SeederDevelopmentOnly
+  def run
+    return unless Rails.env.development?
+
+    Rails.logger.info "Seeding development-only data"
+
+    add_servers
+  end
+
+  private
+
+  def add_servers
+    10.times do
+      FactoryBot.create(:server)
+    end
+  end
+end
+
+SeederAll.new.run
+SeederDevelopmentOnly.new.run
