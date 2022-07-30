beceb67d7aa98e85882c15db0b7f846aa7533722
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2022-07-29 22:15:10 -0300

    Generate devise for admin_users

---
+++ app/models/admin_user.rb
@@ -0,0 +1,14 @@
+class AdminUser < ApplicationRecord
+  # Include default devise modules. Others available are: :omniauthable
+  devise(
+    :database_authenticatable,
+    :registerable,
+    :recoverable,
+    :rememberable,
+    :validatable,
+    :confirmable,
+    :lockable,
+    :timeoutable,
+    :trackable
+  )
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+  devise_for :admin_users, controllers: {
+    sessions: 'admin_users/sessions'
+  }
 
   root to: "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
