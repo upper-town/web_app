a59c90e0e0848c28f654566f7de369d5ba912c5e
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2022-07-31 21:04:49 -0300

    Simplify tests of EmailRecordValidator

--- spec/validators/email_record_validator_spec.rb
+++ spec/validators/email_record_validator_spec.rb
@@ -4,74 +4,19 @@ RSpec.describe EmailRecordValidator do
   describe '#validate' do
     context 'when record has an invalid email' do
       it 'set record.errors' do
-        [
-          'user',
-          'user@',
-          'user@example',
-          'user@example.com1.com2.com3.com4',
-          '.user@@example.com',
-          '_user@@example.com',
-          'user@@example.com',
-          'user#@example.com',
-          'user+test@example.com',
-          'user-test@example.com',
-          'user,test@example.com'
-        ].each do |invalid_email|
-          record = generic_active_record_class.new(email: invalid_email)
-
-          validator = described_class.new(record)
-          validator.validate
-
-          expect(record.errors).not_to be_empty
-          expect(record.errors.messages[:email]).to include('format is not valid')
-        end
-
-        invalid_long_email =
-          'userxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx52@' \
-          'examplexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx52.' \
-          'com1xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx52.' \
-          'com2xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx52.' \
-          'com3xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx52'
-
-        record = generic_active_record_class.new(email: invalid_long_email)
+        record = generic_active_record_class.new(email: 'invalid_email')
 
         validator = described_class.new(record)
         validator.validate
 
         expect(record.errors).not_to be_empty
-        expect(record.errors.messages[:email]).to include('format is not valid')
+        expect(record.errors.messages[:email]).to be_present
       end
     end
 
     context 'when record has a valid email' do
       it 'does not set errors' do
-        [
-          nil,
-          '',
-          " \n  ",
-
-          'user@example.com',
-          'USER@EXAMPLE.COM',
-          'user@example.com1.com2.com3',
-          'user.test@example.com',
-          'user_test@example.com'
-        ].each do |valid_email|
-          record = generic_active_record_class.new(email: valid_email)
-
-          validator = described_class.new(record)
-          validator.validate
-
-          expect(record.errors).to be_empty
-        end
-
-        valid_long_email =
-          'userxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx51@' \
-          'examplexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx51.' \
-          'com1xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx51.' \
-          'com2xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx51.' \
-          'com3xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx51'
-
-        record = generic_active_record_class.new(email: valid_long_email)
+        record = generic_active_record_class.new(email: 'user@google.com')
 
         validator = described_class.new(record)
         validator.validate
@@ -88,7 +33,7 @@ RSpec.describe EmailRecordValidator do
         validator.validate
 
         expect(record.errors).not_to be_empty
-        expect(record.errors.messages[:other]).to include('format is not valid')
+        expect(record.errors.messages[:other]).to be_present
       end
     end
   end
