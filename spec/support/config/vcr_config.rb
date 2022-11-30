8202d7372bb283d7664befa9e696ef175efee41e
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2022-11-30 20:43:29 -0300

    Set more configs in spec/support/vcr_config.rb

--- spec/support/config/vcr_config.rb
+++ spec/support/config/vcr_config.rb
@@ -1,6 +1,13 @@
 VCR.configure do |c|
   c.cassette_library_dir = "spec/cassettes"
   c.hook_into :webmock
+  c.ignore_localhost = true
+  # c.ignore_hosts "chromedriver.storage.googleapis.com" # I'm not sure if this is neeed
+
+  c.default_cassette_options[:record] = :all if ENV['VCR_RECORD_ALL'] == 'true'
 
   c.configure_rspec_metadata!
+
+  # Example of how to filter sensitive values in VCR records
+  # c.filter_sensitive_data('filtered_SOME_ENV_VAR') { ENV.fetch('SOME_ENV_VAR') }
 end
