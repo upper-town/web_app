575574228e941a20103972bcad1771bf25b63826
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2022-12-31 00:41:48 -0300

    List app/components files in config/tailwind.config.js
    Also, unlist .haml and .slim files -- not using them

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/components/**/*.{rb,erb,html}',
+    './app/views/**/*.{erb,html}'
   ],
   theme: {
     extend: {
