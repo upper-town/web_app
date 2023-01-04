8add2765f13269f009d20b35dcbf7ab0649ddfe9
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2023-01-03 21:59:59 -0300

    Add initial version of Admin::TableComponent

---
+++ app/components/admin/table_component.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Admin
+  class TableComponent < ApplicationComponent
+    def initialize(collection: [], columns: [])
+      @collection = collection
+      @columns = columns
+    end
+
+    def render?
+      @collection.any?
+    end
+  end
+end
