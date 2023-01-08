fabe9920c3c49e5136181b2d1f4783bb4895104f
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2023-01-08 12:47:21 -0300

    Define expected classes
    Due to error when running annotate: it expects files to have a class
    defined according to the file path/name

--- app/forms/admin/admin_users/edit_form.rb
+++ app/forms/admin/admin_users/edit_form.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
 
 # TODO
+module Admin
+  module AdminUsers
+    class EditForm
+    end
+  end
+end
--- app/forms/admin/users/edit_form.rb
+++ app/forms/admin/users/edit_form.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
 
 # TODO
+module Admin
+  module Users
+    class EditForm
+    end
+  end
+end
