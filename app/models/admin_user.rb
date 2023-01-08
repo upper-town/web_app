d15d989cded38840f9ab617a545564d2ba03b0e6
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2023-01-08 13:28:54 -0300

    Disable registration paths for admin_users
    They should be registered from the Admin panel

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -36,7 +36,6 @@ class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are: :omniauthable
   devise(
     :database_authenticatable,
-    :registerable,
     :recoverable,
     :rememberable,
     :validatable,
--- config/routes.rb
+++ config/routes.rb
@@ -80,7 +80,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, controllers: {
     confirmations:      'admin_users/confirmations',
     passwords:          'admin_users/passwords',
-    registrations:      'admin_users/registrations',
     sessions:           'admin_users/sessions',
     unlocks:            'admin_users/unlocks',
   }
