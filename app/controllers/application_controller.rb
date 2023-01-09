8700817cff82f851879ff5d764b03103538446f4
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2023-01-08 21:23:41 -0300

    Override Devise's after_sign_in_path_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # Override Devise's after_sign_in_path_for
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(User)
+      root_path
+    elsif resource.is_a?(AdminUser)
+      admin_dashboard_path
+    else
+      super
+    end
+  end
 end
