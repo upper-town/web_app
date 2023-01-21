87b667f914b87646644f8a691bba1d44a058721b
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2023-01-21 20:33:45 -0300

    Update database annotations in spec factory files

--- spec/factories/server_factory.rb
+++ spec/factories/server_factory.rb
@@ -4,19 +4,28 @@
 #
 # Table name: servers
 #
-#  id          :bigint           not null, primary key
-#  description :string           default(""), not null
-#  info        :text             default(""), not null
-#  kind        :string           default(""), not null
-#  name        :string           default(""), not null
-#  site_url    :string           default(""), not null
-#  uuid        :uuid             not null
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id               :bigint           not null, primary key
+#  banner_image_url :string           default(""), not null
+#  country_code     :string           not null
+#  description      :string           default(""), not null
+#  info             :text             default(""), not null
+#  name             :string           not null
+#  site_url         :string           default(""), not null
+#  uuid             :uuid             not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  app_id           :bigint           not null
 #
 # Indexes
 #
-#  index_servers_on_uuid  (uuid) UNIQUE
+#  index_servers_on_app_id           (app_id)
+#  index_servers_on_country_code     (country_code)
+#  index_servers_on_name_and_app_id  (name,app_id) UNIQUE
+#  index_servers_on_uuid             (uuid) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (app_id => apps.id)
 #
 FactoryBot.define do
   factory :server do
--- spec/factories/server_vote_factory.rb
+++ spec/factories/server_vote_factory.rb
@@ -4,21 +4,29 @@
 #
 # Table name: server_votes
 #
-#  id         :bigint           not null, primary key
-#  metadata   :jsonb            not null
-#  uuid       :uuid             not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  server_id  :bigint           not null
+#  id              :bigint           not null, primary key
+#  country_code    :string           not null
+#  metadata        :jsonb            not null
+#  uuid            :uuid             not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  app_id          :bigint           not null
+#  server_id       :bigint           not null
+#  user_account_id :bigint           not null
 #
 # Indexes
 #
-#  index_server_votes_on_server_id  (server_id)
-#  index_server_votes_on_uuid       (uuid) UNIQUE
+#  index_server_votes_on_app_id           (app_id)
+#  index_server_votes_on_country_code     (country_code)
+#  index_server_votes_on_server_id        (server_id)
+#  index_server_votes_on_user_account_id  (user_account_id)
+#  index_server_votes_on_uuid             (uuid) UNIQUE
 #
 # Foreign Keys
 #
+#  fk_rails_...  (app_id => apps.id)
 #  fk_rails_...  (server_id => servers.id)
+#  fk_rails_...  (user_account_id => user_accounts.id)
 #
 FactoryBot.define do
   factory :server_vote do
