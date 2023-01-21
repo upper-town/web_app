47a57d4b3c504ff8fb3d97e7f394c978c6354332
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2023-01-21 20:34:26 -0300

    Add method self.valid? to ShortUuid

--- app/concepts/short_uuid.rb
+++ app/concepts/short_uuid.rb
@@ -8,4 +8,12 @@ class ShortUuid
   def self.to_uuid(short_uuid)
     Base58Id.base58_to_uuid(short_uuid)
   end
+
+  def self.valid?(short_uuid)
+    if short_uuid.blank?
+      false
+    else
+      Base58Id.valid_base58?(short_uuid)
+    end
+  end
 end
