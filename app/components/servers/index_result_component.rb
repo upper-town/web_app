1c7b4ffcb75b1a99750cf67b7b7a4c7aa04ee972
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2023-01-21 20:42:37 -0300

    Add initial IndexResultComponent for servers#index

--- app/components/servers/index_result_component.rb
+++ app/components/servers/index_result_component.rb
@@ -2,12 +2,41 @@
 
 module Servers
   class IndexResultComponent < ApplicationComponent
-    def initialize(server:)
+    def initialize(server:, server_stats_hash:, period:, country_code:)
       @server = server
+      @server_stats_hash = server_stats_hash
+      @period = period
+      @country_code = country_code
     end
 
     def render?
       @server.present?
+    end
+
+    def format_ranking_number(value)
+      number = format_number(value)
+      '#' + (number.nil? ? '--' : number)
+    end
+
+    def format_vote_count(value)
+      number = format_number(value)
+      number.nil? ? '--' : number
+    end
+
+    private
+
+    def format_number(value)
+      if value.nil? || value.negative?
+        nil
+      elsif value < 100_000
+        number_with_delimiter(value)
+      else
+        number_to_human(
+          value,
+          precision: 4,
+          units: { thousand: 'k', million: 'M', billion: 'G', trillion: 'T' }
+        )
+      end
     end
   end
 end
