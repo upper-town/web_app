c0a539d4a9fbfe85320c21e3fbb0b609e4847910
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2023-02-05 20:09:55 -0300

    Add routes for server_votes

--- config/routes.rb
+++ config/routes.rb
@@ -2,69 +2,73 @@
 
 # == Route Map
 #
-#                            Prefix Verb   URI Pattern                             Controller#Action
-#                  new_user_session GET    /users/sign_in(.:format)                users/sessions#new
-#                      user_session POST   /users/sign_in(.:format)                users/sessions#create
-#              destroy_user_session DELETE /users/sign_out(.:format)               users/sessions#destroy
-#                 new_user_password GET    /users/password/new(.:format)           users/passwords#new
-#                edit_user_password GET    /users/password/edit(.:format)          users/passwords#edit
-#                     user_password PATCH  /users/password(.:format)               users/passwords#update
-#                                   PUT    /users/password(.:format)               users/passwords#update
-#                                   POST   /users/password(.:format)               users/passwords#create
-#          cancel_user_registration GET    /users/cancel(.:format)                 users/registrations#cancel
-#             new_user_registration GET    /users/sign_up(.:format)                users/registrations#new
-#            edit_user_registration GET    /users/edit(.:format)                   users/registrations#edit
-#                 user_registration PATCH  /users(.:format)                        users/registrations#update
-#                                   PUT    /users(.:format)                        users/registrations#update
-#                                   DELETE /users(.:format)                        users/registrations#destroy
-#                                   POST   /users(.:format)                        users/registrations#create
-#             new_user_confirmation GET    /users/confirmation/new(.:format)       users/confirmations#new
-#                 user_confirmation GET    /users/confirmation(.:format)           users/confirmations#show
-#                                   POST   /users/confirmation(.:format)           users/confirmations#create
-#                   new_user_unlock GET    /users/unlock/new(.:format)             users/unlocks#new
-#                       user_unlock GET    /users/unlock(.:format)                 users/unlocks#show
-#                                   POST   /users/unlock(.:format)                 users/unlocks#create
-#            new_admin_user_session GET    /admin_users/sign_in(.:format)          admin_users/sessions#new
-#                admin_user_session POST   /admin_users/sign_in(.:format)          admin_users/sessions#create
-#        destroy_admin_user_session DELETE /admin_users/sign_out(.:format)         admin_users/sessions#destroy
-#           new_admin_user_password GET    /admin_users/password/new(.:format)     admin_users/passwords#new
-#          edit_admin_user_password GET    /admin_users/password/edit(.:format)    admin_users/passwords#edit
-#               admin_user_password PATCH  /admin_users/password(.:format)         admin_users/passwords#update
-#                                   PUT    /admin_users/password(.:format)         admin_users/passwords#update
-#                                   POST   /admin_users/password(.:format)         admin_users/passwords#create
-#       new_admin_user_confirmation GET    /admin_users/confirmation/new(.:format) admin_users/confirmations#new
-#           admin_user_confirmation GET    /admin_users/confirmation(.:format)     admin_users/confirmations#show
-#                                   POST   /admin_users/confirmation(.:format)     admin_users/confirmations#create
-#             new_admin_user_unlock GET    /admin_users/unlock/new(.:format)       admin_users/unlocks#new
-#                 admin_user_unlock GET    /admin_users/unlock(.:format)           admin_users/unlocks#show
-#                                   POST   /admin_users/unlock(.:format)           admin_users/unlocks#create
-#                              root GET    /                                       home#index
-#                             admin GET    /admin(.:format)                        redirect(301, /admin/dashboard)
-#                   admin_dashboard GET    /admin/dashboard(.:format)              admin/dashboards#show
-#                       admin_users GET    /admin/users(.:format)                  admin/users#index
-#                        admin_user GET    /admin/users/:id(.:format)              admin/users#show
-#                 admin_admin_users GET    /admin/admin_users(.:format)            admin/admin_users#index
-#                                   POST   /admin/admin_users(.:format)            admin/admin_users#create
-#              new_admin_admin_user GET    /admin/admin_users/new(.:format)        admin/admin_users#new
-#             edit_admin_admin_user GET    /admin/admin_users/:id/edit(.:format)   admin/admin_users#edit
-#                  admin_admin_user GET    /admin/admin_users/:id(.:format)        admin/admin_users#show
-#                                   PATCH  /admin/admin_users/:id(.:format)        admin/admin_users#update
-#                                   PUT    /admin/admin_users/:id(.:format)        admin/admin_users#update
-#                                   DELETE /admin/admin_users/:id(.:format)        admin/admin_users#destroy
-#                     admin_servers GET    /admin/servers(.:format)                admin/servers#index
-#                                   POST   /admin/servers(.:format)                admin/servers#create
-#                  new_admin_server GET    /admin/servers/new(.:format)            admin/servers#new
-#                 edit_admin_server GET    /admin/servers/:id/edit(.:format)       admin/servers#edit
-#                      admin_server GET    /admin/servers/:id(.:format)            admin/servers#show
-#                                   PATCH  /admin/servers/:id(.:format)            admin/servers#update
-#                                   PUT    /admin/servers/:id(.:format)            admin/servers#update
-#                                   DELETE /admin/servers/:id(.:format)            admin/servers#destroy
-#                           servers GET    /servers(.:format)                      servers#index
-#                            server GET    /servers/:suuid(.:format)               servers#show
-#                       sidekiq_web        /admin/sidekiq                          Sidekiq::Web
-#  turbo_recede_historical_location GET    /recede_historical_location(.:format)   turbo/native/navigation#recede
-#  turbo_resume_historical_location GET    /resume_historical_location(.:format)   turbo/native/navigation#resume
-# turbo_refresh_historical_location GET    /refresh_historical_location(.:format)  turbo/native/navigation#refresh
+#                            Prefix Verb   URI Pattern                                Controller#Action
+#                  new_user_session GET    /users/sign_in(.:format)                   users/sessions#new
+#                      user_session POST   /users/sign_in(.:format)                   users/sessions#create
+#              destroy_user_session DELETE /users/sign_out(.:format)                  users/sessions#destroy
+#                 new_user_password GET    /users/password/new(.:format)              users/passwords#new
+#                edit_user_password GET    /users/password/edit(.:format)             users/passwords#edit
+#                     user_password PATCH  /users/password(.:format)                  users/passwords#update
+#                                   PUT    /users/password(.:format)                  users/passwords#update
+#                                   POST   /users/password(.:format)                  users/passwords#create
+#          cancel_user_registration GET    /users/cancel(.:format)                    users/registrations#cancel
+#             new_user_registration GET    /users/sign_up(.:format)                   users/registrations#new
+#            edit_user_registration GET    /users/edit(.:format)                      users/registrations#edit
+#                 user_registration PATCH  /users(.:format)                           users/registrations#update
+#                                   PUT    /users(.:format)                           users/registrations#update
+#                                   DELETE /users(.:format)                           users/registrations#destroy
+#                                   POST   /users(.:format)                           users/registrations#create
+#             new_user_confirmation GET    /users/confirmation/new(.:format)          users/confirmations#new
+#                 user_confirmation GET    /users/confirmation(.:format)              users/confirmations#show
+#                                   POST   /users/confirmation(.:format)              users/confirmations#create
+#                   new_user_unlock GET    /users/unlock/new(.:format)                users/unlocks#new
+#                       user_unlock GET    /users/unlock(.:format)                    users/unlocks#show
+#                                   POST   /users/unlock(.:format)                    users/unlocks#create
+#            new_admin_user_session GET    /admin_users/sign_in(.:format)             admin_users/sessions#new
+#                admin_user_session POST   /admin_users/sign_in(.:format)             admin_users/sessions#create
+#        destroy_admin_user_session DELETE /admin_users/sign_out(.:format)            admin_users/sessions#destroy
+#           new_admin_user_password GET    /admin_users/password/new(.:format)        admin_users/passwords#new
+#          edit_admin_user_password GET    /admin_users/password/edit(.:format)       admin_users/passwords#edit
+#               admin_user_password PATCH  /admin_users/password(.:format)            admin_users/passwords#update
+#                                   PUT    /admin_users/password(.:format)            admin_users/passwords#update
+#                                   POST   /admin_users/password(.:format)            admin_users/passwords#create
+#       new_admin_user_confirmation GET    /admin_users/confirmation/new(.:format)    admin_users/confirmations#new
+#           admin_user_confirmation GET    /admin_users/confirmation(.:format)        admin_users/confirmations#show
+#                                   POST   /admin_users/confirmation(.:format)        admin_users/confirmations#create
+#             new_admin_user_unlock GET    /admin_users/unlock/new(.:format)          admin_users/unlocks#new
+#                 admin_user_unlock GET    /admin_users/unlock(.:format)              admin_users/unlocks#show
+#                                   POST   /admin_users/unlock(.:format)              admin_users/unlocks#create
+#                              root GET    /                                          home#index
+#                             admin GET    /admin(.:format)                           redirect(301, /admin/dashboard)
+#                   admin_dashboard GET    /admin/dashboard(.:format)                 admin/dashboards#show
+#                       admin_users GET    /admin/users(.:format)                     admin/users#index
+#                        admin_user GET    /admin/users/:id(.:format)                 admin/users#show
+#                 admin_admin_users GET    /admin/admin_users(.:format)               admin/admin_users#index
+#                                   POST   /admin/admin_users(.:format)               admin/admin_users#create
+#              new_admin_admin_user GET    /admin/admin_users/new(.:format)           admin/admin_users#new
+#             edit_admin_admin_user GET    /admin/admin_users/:id/edit(.:format)      admin/admin_users#edit
+#                  admin_admin_user GET    /admin/admin_users/:id(.:format)           admin/admin_users#show
+#                                   PATCH  /admin/admin_users/:id(.:format)           admin/admin_users#update
+#                                   PUT    /admin/admin_users/:id(.:format)           admin/admin_users#update
+#                                   DELETE /admin/admin_users/:id(.:format)           admin/admin_users#destroy
+#                     admin_servers GET    /admin/servers(.:format)                   admin/servers#index
+#                                   POST   /admin/servers(.:format)                   admin/servers#create
+#                  new_admin_server GET    /admin/servers/new(.:format)               admin/servers#new
+#                 edit_admin_server GET    /admin/servers/:id/edit(.:format)          admin/servers#edit
+#                      admin_server GET    /admin/servers/:id(.:format)               admin/servers#show
+#                                   PATCH  /admin/servers/:id(.:format)               admin/servers#update
+#                                   PUT    /admin/servers/:id(.:format)               admin/servers#update
+#                                   DELETE /admin/servers/:id(.:format)               admin/servers#destroy
+#                      server_votes GET    /servers/:server_suuid/votes(.:format)     server_votes#index
+#                                   POST   /servers/:server_suuid/votes(.:format)     server_votes#create
+#                   new_server_vote GET    /servers/:server_suuid/votes/new(.:format) server_votes#new
+#                           servers GET    /servers(.:format)                         servers#index
+#                            server GET    /servers/:suuid(.:format)                  servers#show
+#                       server_vote GET    /server_votes/:id(.:format)                server_votes#show
+#                       sidekiq_web        /admin/sidekiq                             Sidekiq::Web
+#  turbo_recede_historical_location GET    /recede_historical_location(.:format)      turbo/native/navigation#recede
+#  turbo_resume_historical_location GET    /resume_historical_location(.:format)      turbo/native/navigation#resume
+# turbo_refresh_historical_location GET    /refresh_historical_location(.:format)     turbo/native/navigation#refresh
 
 require 'sidekiq/web'
 require 'sidekiq/cron/web'
@@ -99,7 +103,10 @@ Rails.application.routes.draw do
     resources :servers, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   end
 
-  resources :servers, only: [:index, :show], param: :suuid
+  resources :servers, only: [:index, :show], param: :suuid do
+    resources :server_votes, as: 'votes', path: 'votes', only: [:index, :new, :create]
+  end
+  resources :server_votes, only: [:show], param: :suuid
 
   # Sidekiq
 
