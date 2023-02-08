2cc70df06a4788013020d1c9047de747d88d4ff8
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2023-02-08 17:54:22 -0300

    Update ServeVotes::NewForm with Captcha and explicit URL and http method

--- app/forms/server_votes/new_form.rb
+++ app/forms/server_votes/new_form.rb
@@ -5,5 +5,13 @@ module ServerVotes
     attribute :reference_id, :string, default: ''
 
     validates :reference_id, length: { maximum: 255 }
+
+    def method
+      :post
+    end
+
+    def url(server)
+      server_votes_path(server.suuid)
+    end
   end
 end
