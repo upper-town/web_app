562724bdd3aa79b5331f5b205a6cbdde34e8d73b
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2023-02-08 18:37:41 -0300

    Set custom localhost name for development and test
    Add to your /etc/hosts file
    
        127.0.1.1  upper.town.localhost

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,7 +62,9 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  default_url_options = { host: 'localhost', port: ENV.fetch('PORT') }
+  config.hosts << ENV.fetch('HOST')
+
+  default_url_options = { host: ENV.fetch('HOST'), port: ENV.fetch('PORT') }
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -50,7 +50,9 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
-  default_url_options = { host: 'localhost', port: ENV.fetch('PORT') }
+  config.hosts << ENV.fetch('HOST')
+
+  default_url_options = { host: ENV.fetch('HOST'), port: ENV.fetch('PORT') }
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
