319f29de21de134312cce03cfc6c1048adf5a4fd
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2023-03-12 19:45:15 -0300

    Initialize UserAccount when User is created

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -81,23 +81,27 @@ module Users
 
     # Based on Devise::RegistrationsController#create
     def devise_registrations_create
-      resource.uuid = SecureRandom.uuid
-      resource.save
+      ActiveRecord::Base.transaction do
+        resource.uuid = SecureRandom.uuid
+        resource.save
 
-      if resource.persisted?
-        if resource.active_for_authentication?
-          set_flash_message!(:notice, :signed_up)
-          sign_up(resource_name, resource)
-          respond_with(resource, location: after_sign_up_path_for(resource))
+        if resource.persisted?
+          Users::InitializeAccount.new(resource).call
+
+          if resource.active_for_authentication?
+            set_flash_message!(:notice, :signed_up)
+            sign_up(resource_name, resource)
+            respond_with(resource, location: after_sign_up_path_for(resource))
+          else
+            set_flash_message!(:notice, :"signed_up_but_#{resource.inactive_message}")
+            expire_data_after_sign_in!
+            respond_with(resource, location: after_inactive_sign_up_path_for(resource))
+          end
         else
-          set_flash_message!(:notice, :"signed_up_but_#{resource.inactive_message}")
-          expire_data_after_sign_in!
-          respond_with(resource, location: after_inactive_sign_up_path_for(resource))
+          clean_up_passwords(resource)
+          set_minimum_password_length
+          respond_with(resource)
         end
-      else
-        clean_up_passwords(resource)
-        set_minimum_password_length
-        respond_with(resource)
       end
     end
   end
