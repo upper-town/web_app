cfb59bf70bd235d8855270b9f8e5a1d5223b12aa
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2023-03-12 19:44:10 -0300

    Improve ShortUuid.valid? checks

--- app/concepts/short_uuid.rb
+++ app/concepts/short_uuid.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class ShortUuid
+  MAX_SHORT_UUID_STR_SIZE = 22 # hmep7uZkFTa9zuEuQB3XV5
+  MAX_UUID_INTEGER_VALUE = (2**128) - 1 # ffffffff-ffff-ffff-ffff-ffffffffffff
+
   def self.from_uuid(uuid)
     Base58Id.uuid_to_base58(uuid)
   end
@@ -10,10 +13,11 @@ class ShortUuid
   end
 
   def self.valid?(short_uuid)
-    if short_uuid.blank?
-      false
-    else
-      Base58Id.valid_base58?(short_uuid)
-    end
+    return false if short_uuid.blank?
+    return false if short_uuid.size > MAX_SHORT_UUID_STR_SIZE
+    return false if !Base58Id.valid_base58?(short_uuid)
+    return false if Base58Id.base58_to_integer(short_uuid) > MAX_UUID_INTEGER_VALUE
+
+    true
   end
 end
