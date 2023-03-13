b244cf06f7625a4626c798cd11eb3959b2ab1c2e
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2023-03-13 18:48:08 -0300

    Fix queries for servers#index
    Conditions should be in the JOIN, not in the WHERE clause

--- app/concepts/servers/index_query.rb
+++ app/concepts/servers/index_query.rb
@@ -13,68 +13,41 @@ module Servers
       scope = Server.includes(:app)
       scope = scope.where(app: @app) if @app.present?
       scope = scope.where(country_code: @country_code) if Server::COUNTRY_CODES.include?(@country_code)
-
-      scope = scope.joins(sql_left_join)
-      scope = scope.where(sql_conditions_period_reference_date)
-      scope = scope.where(sql_conditions_app_id)
-      scope = scope.where(sql_conditions_country_code)
+      scope = scope.joins(sql_left_join_server_stats)
 
       scope.order(sql_order)
     end
 
     private
 
-    def sql_left_join
+    def sql_left_join_server_stats
       <<-SQL.squish
-        LEFT OUTER JOIN "server_stats" ON
-          "server_stats"."server_id" = "servers"."id"
+        LEFT JOIN "server_stats" ON
+              "server_stats"."server_id" = "servers"."id"
+          AND "server_stats"."app_id"    = "servers"."app_id"
+          AND #{sql_on_period_and_reference_date}
+          AND #{sql_on_country_code}
       SQL
     end
 
-    def sql_conditions_period_reference_date
+    def sql_on_period_and_reference_date
       <<-SQL.squish
-        (
-          "server_stats"."period" = #{quote_for_sql(@period)} AND
-          "server_stats"."reference_date" = #{quote_for_sql(ServerStat.reference_date_for(@period, @current_time))}
-        ) OR (
-          "server_stats"."period" IS NULL AND
-          "server_stats"."reference_date" IS NULL
-        )
+            "server_stats"."period"         = #{quote_for_sql(@period)}
+        AND "server_stats"."reference_date" = #{quote_for_sql(ServerStat.reference_date_for(@period, @current_time))}
       SQL
     end
 
-    def sql_conditions_app_id
+    def sql_on_country_code
       <<-SQL.squish
-        (
-          "server_stats"."app_id" = "servers"."app_id" OR
-          "server_stats"."app_id" IS NULL
-        )
+        "server_stats"."country_code" = #{
+          @country_code == ServerStat::GLOBAL ? quote_for_sql(ServerStat::GLOBAL) : '"servers"."country_code"'
+        }
       SQL
-    end
-
-    def sql_conditions_country_code
-      if @country_code == ServerStat::GLOBAL
-        <<-SQL.squish
-          (
-            "server_stats"."country_code" = #{quote_for_sql(ServerStat::GLOBAL)} OR
-            "server_stats"."country_code" IS NULL
-          )
-        SQL
-      else
-        <<-SQL.squish
-          (
-            "server_stats"."country_code" = "servers"."country_code" OR
-            "server_stats"."country_code" IS NULL
-          )
-        SQL
-      end
     end
 
     def sql_order
       <<-SQL.squish
-        "server_stats"."period",
-        "server_stats"."ranking_number" ASC,
-        "servers"."app_id" ASC
+        "server_stats"."ranking_number" ASC
       SQL
     end
 
