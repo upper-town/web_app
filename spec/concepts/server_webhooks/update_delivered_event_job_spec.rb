3bd3683ae55653c44a5275a9b247a3a84f8394ee
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2023-03-21 22:49:29 -0300

    Add unit test for ServerWebhooks::UpdateDeliveredEventJob

---
+++ spec/concepts/server_webhooks/update_delivered_event_job_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ServerWebhooks::UpdateDeliveredEventJob do
+  describe '#perform' do
+    context 'when a ServerWebhookEvent cannot be found with server_webhook_event_id' do
+      it 'raises an error' do
+        expect do
+          described_class.new.perform(0)
+        end.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context 'when a ServerWebhookEvent is found with server_webhook_event_id' do
+      it 'updates its notice and status' do
+        server_webhook_event = create(
+          :server_webhook_event,
+          notice: 'Some previous notice',
+          status: ServerWebhookEvent::RETRY
+        )
+
+        described_class.new.perform(server_webhook_event.id)
+
+        server_webhook_event.reload
+        expect(server_webhook_event.notice).to eq('')
+        expect(server_webhook_event.status).to eq(ServerWebhookEvent::DELIVERED)
+      end
+    end
+  end
+end
