0865206a5c144cd5c17d5c9534c8ae9ac397b886
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2023-03-22 16:15:23 -0300

    Add ServerWebhooks::DeleteOldEventsJob

---
+++ app/concepts/server_webhooks/delete_old_events_job.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module ServerWebhooks
+  class DeleteOldEventsJob
+    include Sidekiq::Job
+
+    sidekiq_options(lock: :while_executing)
+
+    def perform
+      old_server_webhook_events_query.delete_all
+    end
+
+    private
+
+    def old_server_webhook_events_query
+      ServerWebhookEvent.where(
+        status: [
+          ServerWebhookEvent::FAILED,
+          ServerWebhookEvent::DELIVERED
+        ],
+        updated_at: ..(3.months.ago)
+      )
+    end
+  end
+end
