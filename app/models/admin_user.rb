c7bdc92647391542b378d380a02a4d244f5a3453
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2024-05-11 10:28:35 -0300

    Remove Auth::AuthenticationModel

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -29,17 +29,135 @@
 #  index_admin_users_on_email  (email) UNIQUE
 #
 class AdminUser < ApplicationRecord
+  TOKEN_EXPIRATION = 1.hour
+  TOKEN_LENGTH     = 24
+
   require 'string_value_helper'
 
-  include Auth::AuthenticationModel
   include FeatureFlagIdModel
 
   has_many :active_sessions, class_name: 'AdminUserActiveSession', dependent: :destroy
   has_many :tokens, class_name: 'AdminUserToken', dependent: :destroy
   has_one :account, class_name: 'AdminUserAccount', dependent: :destroy
 
+  has_secure_password validations: false
+
+  normalizes :email, with: EmailNormalizer
+  normalizes :change_email, with: EmailNormalizer
+
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
+  validates :password, length: { minimum: 8 }, allow_blank: true
+
+  validate do |record|
+    EmailRecordValidator.new(record).validate
+  end
+
+  def self.find_by_token(purpose, token)
+    return if purpose.blank? || token.blank?
+
+    # TODO: test this
+    joins(:tokens)
+      .where(tokens: { purpose: purpose, token: token })
+      .where('tokens.expires_at > ?', Time.current)
+      .order(created_at: :desc)
+      .first
+  end
+
   # Super Admin status can only be granted through env var.
   def super_admin?
     StringValueHelper.values_list_uniq(ENV.fetch('SUPER_ADMIN_USER_EMAILS'), '').include?(email)
+  end
+
+  def regenerate_token!(purpose, expires_in = nil, data = {})
+    expires_in ||= TOKEN_EXPIRATION
+
+    token = tokens.create!(
+      purpose: purpose,
+      token: SecureRandom.base58(TOKEN_LENGTH),
+      expires_at: expires_in.from_now,
+      data: data
+    )
+
+    token.token
+  end
+
+  def current_token(purpose)
+    tokens
+      .where(purpose: purpose)
+      .order(created_at: :desc)
+      .first&.token
+  end
+
+  def confirmed_email?
+    email_confirmed_at.present?
+  end
+
+  def unconfirmed_email?
+    !confirmed_email?
+  end
+
+  def confirm_email!
+    update!(email_confirmed_at: Time.current)
+  end
+
+  def unconfirm_email!
+    update!(email_confirmed_at: nil)
+  end
+
+  def confirmed_change_email?
+    change_email_confirmed_at.present?
+  end
+
+  def unconfirmed_change_email?
+    !confirmed_change_email?
+  end
+
+  def confirm_change_email!
+    update!(change_email_confirmed_at: Time.current)
+  end
+
+  def unconfirm_change_email!
+    update!(change_email_confirmed_at: nil)
+  end
+
+  def revert_change_email!(previous_email)
+    update!(
+      email: previous_email,
+      email_confirmed_at: Time.current,
+      change_email: nil,
+      change_email_confirmed_at: nil,
+      change_email_reverted_at: Time.current
+    )
+  end
+
+  def locked?
+    locked_at.present?
+  end
+
+  def unlocked?
+    !locked?
+  end
+
+  def lock!(reason, comment = nil)
+    update!(
+      locked_reason:  reason,
+      locked_comment: comment,
+      locked_at:      Time.current
+    )
+  end
+
+  def unlock!
+    update!(
+      locked_reason:  nil,
+      locked_comment: nil,
+      locked_at:      nil,
+    )
+  end
+
+  def reset_password!(password)
+    update!(
+      password:          password,
+      password_reset_at: Time.current
+    )
   end
 end
