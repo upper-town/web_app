6ad3ca32b47d09d42362cf937816ec23c796b2e1
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2024-08-09 14:21:52 -0300

    Add equal_id and equal_attributes to ApplicationModel

--- app/models/application_model.rb
+++ app/models/application_model.rb
@@ -9,10 +9,23 @@ class ApplicationModel
   include Rails.application.routes.url_helpers
 
   def ==(other)
-    super || (
-      other.instance_of?(self.class) &&
-        other.attributes['id'].present? && attributes['id'].present? &&
-        other.id == id
-    )
+    super || equal_id(other) || equal_attributes(other)
+  end
+
+  private
+
+  def equal_id(other)
+    equal_class(other) &&
+      other.attributes['id'].present? && attributes['id'].present? &&
+      other.id == id
+  end
+
+  def equal_attributes(other)
+    equal_class(other) &&
+      other.attributes == attributes
+  end
+
+  def equal_class(other)
+    other.instance_of?(self.class)
   end
 end
