f916b260e3580649700b0af0fab7a626c64f73aa
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2024-08-18 19:01:18 -0300

    Add specs for AdminRole

--- app/models/admin_role.rb
+++ app/models/admin_role.rb
@@ -21,5 +21,9 @@ class AdminRole < ApplicationRecord
   has_many :accounts, through: :admin_account_roles, source: :admin_account
   has_many :permissions, through: :admin_role_permissions, source: :admin_permission
 
-  validates :key, :description, presence: true
+  normalizes :key, with: ->(str) { str.downcase.squish.tr(' ', '_') }
+  normalizes :description, with: ->(str) { str.squish }
+
+  validates :key, presence: true, uniqueness: { case_sensitive: false }
+  validates :description, presence: true
 end
