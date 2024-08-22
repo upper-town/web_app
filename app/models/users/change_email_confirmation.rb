de99209d75af34cfee35c528268019cf0d1bed11
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2024-08-21 23:06:13 -0300

    Add specs for Users::ChangeEmailConfirmation

--- app/models/users/change_email_confirmation.rb
+++ app/models/users/change_email_confirmation.rb
@@ -2,13 +2,13 @@
 
 module Users
   class ChangeEmailConfirmation < ApplicationModel
-    attribute :email, :string
-    attribute :change_email, :string
-    attribute :password, :string
+    attribute :email, :string, default: nil
+    attribute :change_email, :string, default: nil
+    attribute :password, :string, default: nil
 
-    validates :email, presence: true
-    validates :change_email, presence: true, email: true
-    validates :password, presence: true
+    validates :email, presence: true, length: { minimum: 3, maximum: 255 }
+    validates :change_email, presence: true, length: { minimum: 3, maximum: 255 }
+    validates :password, presence: true, length: { maximum: 255 }
 
     def email=(value)
       super(NormalizeEmail.call(value))
