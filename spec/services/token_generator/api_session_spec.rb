e02101d1e9a9b95677d427e14911b6ce05e664e1
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2024-08-21 22:28:42 -0300

    Add specs for TokenGenerator

---
+++ spec/services/token_generator/api_session_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe TokenGenerator::ApiSession do
+  let(:base58_chars) { 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz123456789' }
+
+  describe '.generate' do
+    it 'generates a random token and returns it with digest and last four' do
+      secret = Rails.application.key_generator.generate_key(ENV.fetch('TOKEN_API_SESSION_SALT'))
+
+      token, token_digest, token_last_four = described_class.generate
+      expect(token).to match(/\A[#{base58_chars}]{48}\z/)
+      expect(token_digest).to eq(OpenSSL::HMAC.hexdigest('sha256', secret, token))
+      expect(token_last_four).to eq(token.last(4))
+    end
+  end
+
+  describe '.digest' do
+    it 'returns HMAC-signed digest of a token' do
+      secret = Rails.application.key_generator.generate_key(ENV.fetch('TOKEN_API_SESSION_SALT'))
+      token = 'abcdef123456'
+      expect_token_digest = OpenSSL::HMAC.hexdigest('sha256', secret, token)
+
+      token_digest = described_class.digest(token)
+
+      expect(token_digest).to eq(expect_token_digest)
+    end
+  end
+end
---
+++ spec/services/token_generator_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe TokenGenerator do
+  let(:base58_chars) { 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz123456789' }
+
+  describe '.generate' do
+    it 'generates a random token and returns it with digest and last four' do
+      secret = Rails.application.key_generator.generate_key(ENV.fetch('TOKEN_SALT'))
+
+      token, token_digest, token_last_four = described_class.generate
+      expect(token).to match(/\A[#{base58_chars}]{48}\z/)
+      expect(token_digest).to eq(OpenSSL::HMAC.hexdigest('sha256', secret, token))
+      expect(token_last_four).to eq(token.last(4))
+
+      token, token_digest, token_last_four = described_class.generate(10)
+      expect(token).to match(/\A[#{base58_chars}]{10}\z/)
+      expect(token_digest).to eq(OpenSSL::HMAC.hexdigest('sha256', secret, token))
+      expect(token_last_four).to eq(token.last(4))
+
+      secret = 'aaaaaaaa'
+
+      token, token_digest, token_last_four = described_class.generate(20, secret)
+      expect(token).to match(/\A[#{base58_chars}]{20}\z/)
+      expect(token_digest).to eq(OpenSSL::HMAC.hexdigest('sha256', secret, token))
+      expect(token_last_four).to eq(token.last(4))
+    end
+  end
+
+  describe '.digest' do
+    it 'returns HMAC-signed digest of a token' do
+      secret = Rails.application.key_generator.generate_key(ENV.fetch('TOKEN_SALT'))
+      token = 'abcdef123456'
+      expect_token_digest = OpenSSL::HMAC.hexdigest('sha256', secret, token)
+
+      token_digest = described_class.digest(token)
+
+      expect(token_digest).to eq(expect_token_digest)
+    end
+  end
+end
