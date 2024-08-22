c5c5c3afd0dcbf2715fb7880ea73e1b62caf09ae
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2024-08-21 23:22:26 -0300

    Add specs for Users::EmailConfirmationEdit

---
+++ spec/models/users/email_confirmation_edit_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Users::EmailConfirmationEdit do
+  it 'has default values' do
+    instance = described_class.new
+
+    expect(instance.token).to be_nil
+    expect(instance.auto_click).to be(false)
+  end
+
+  describe 'validations' do
+    it 'validates token' do
+      instance = described_class.new(token: ' ')
+      instance.validate
+      expect(instance.errors.of_kind?(:token, :blank)).to be(true)
+
+      instance = described_class.new(token: 'a' * 256)
+      instance.validate
+      expect(instance.errors.of_kind?(:token, :too_long)).to be(true)
+
+      instance = described_class.new(token: 'abcdef123456')
+      instance.validate
+      expect(instance.errors.key?(:token)).to be(false)
+    end
+  end
+
+  describe 'normalizations' do
+    it 'normalizes token' do
+      instance = described_class.new(token: nil)
+
+      expect(instance.token).to be_nil
+
+      instance = described_class.new(token: "\n\t Aaaa1234 B  bbb 5678\n")
+
+      expect(instance.token).to eq('Aaaa1234Bbbb5678')
+    end
+  end
+end
