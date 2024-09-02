23530a78879087f9557f86f7a14cb526a12418c9
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2024-09-02 11:12:54 -0300

    Add specs for ServerWebhooks::BuildEventRequestHeadersAndBody

--- app/concepts/server_webhooks/build_event_request_headers_and_body.rb
+++ app/concepts/server_webhooks/build_event_request_headers_and_body.rb
@@ -2,8 +2,6 @@
 
 module ServerWebhooks
   class BuildEventRequestHeadersAndBody
-    SIGNATURE_HEADER = 'X-Upper-Town-Server-Webhook-Signature'
-
     attr_reader :server_webhook_event
 
     def initialize(server_webhook_event)
@@ -12,8 +10,9 @@ module ServerWebhooks
 
     def call
       request_body = build_request_body
-      request_signatures = build_request_signatures(request_body)
-      request_headers = build_request_headers(request_signatures)
+      request_signature = build_request_signature(request_body)
+      request_other_signature = build_request_other_signature(request_body)
+      request_headers = build_request_headers(request_signature, request_other_signature)
 
       [request_headers, request_body]
     end
@@ -22,50 +21,38 @@ module ServerWebhooks
 
     def build_request_body
       {
-        'event' => {
+        'webhook_event' => {
           'type'              => server_webhook_event.type,
-          'last_published_at' => server_webhook_event.last_published_at,
-          'failed_attempts'   => server_webhook_event.failed_attempts,
           'payload'           => server_webhook_event.payload,
+          'last_published_at' => server_webhook_event.last_published_at.iso8601,
+          'failed_attempts'   => server_webhook_event.failed_attempts,
         }
       }.to_json
     end
 
-    def build_request_signatures(request_body)
-      active_signatures = active_server_webhook_secrets_query.map do |value|
-        generate_signature(value, request_body)
-      end
+    def build_request_signature(request_body)
+      secret = server_webhook_event.config.secret
 
-      archived_signatures = archived_server_webhook_secrets_query.map do |value|
-        generate_signature(value, request_body)
-      end
-
-      active_signatures + archived_signatures
+      generate_signature(secret, request_body)
     end
 
-    def build_request_headers(request_signatures)
+    def build_request_other_signature(request_body)
+      other_secret = server_webhook_event.config.other_secret
+      return if other_secret.blank?
+
+      generate_signature(other_secret, request_body)
+    end
+
+    def build_request_headers(request_signature, request_other_signature)
       {
-        'Content-Type'   => 'application/json',
-        SIGNATURE_HEADER => request_signatures.join(',')
+        'Content-Type'      => 'application/json',
+        'X-Signature'       => request_signature,
+        'X-Other-Signature' => request_other_signature,
       }.compact_blank
     end
 
     def generate_signature(secret, request_body)
       OpenSSL::HMAC.hexdigest('sha256', secret, request_body)
-    end
-
-    def active_server_webhook_secrets_query
-      ServerWebhookSecret
-        .active
-        .where(server_id: server_webhook_event.server_id)
-        .pluck(:value)
-    end
-
-    def archived_server_webhook_secrets_query
-      ServerWebhookSecret
-        .archived
-        .where(server_id: server_webhook_event.server_id)
-        .pluck(:value)
     end
   end
 end
--- spec/concepts/server_webhooks/build_event_request_headers_and_body_spec.rb
+++ spec/concepts/server_webhooks/build_event_request_headers_and_body_spec.rb
@@ -2,77 +2,76 @@
 
 require 'rails_helper'
 
-# rubocop:disable Layout/LineLength
 RSpec.describe ServerWebhooks::BuildEventRequestHeadersAndBody do
   describe '#call' do
-    context 'when server does not have any ServerWebhookSecrets' do
-      it 'returns request_headers without SIGNATURE_HEADER, and returns request_body accordingly' do
+    it 'returns request_headers and request_body accordingly' do
+      server = create(:server)
+      server_webhook_config = create(
+        :server_webhook_config,
+        server: server,
+        secret: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+      )
+      server_webhook_event = create(
+        :server_webhook_event,
+        server: server,
+        config: server_webhook_config,
+        type: 'test.event',
+        last_published_at: '2024-09-02T12:00:01Z',
+        failed_attempts: 1,
+        payload: { 'server_vote' => { 'uuid' => '11111111-1111-1111-1111-111111111111' } }
+      )
+
+      request_headers, request_body = described_class.new(server_webhook_event).call
+
+      expect(request_headers).to eq({
+        'Content-Type' => 'application/json',
+        'X-Signature'  => '42026a10921d68149b440c2a5d718e65c2259a0d8acbb6c7087fe993040400a6'
+      })
+      expect(request_body).to eq({
+        'webhook_event' => {
+          'type'              => 'test.event',
+          'payload'           => { 'server_vote' => { 'uuid' => '11111111-1111-1111-1111-111111111111' } },
+          'last_published_at' => '2024-09-02T12:00:01Z',
+          'failed_attempts'   => 1,
+        }
+      }.to_json)
+    end
+
+    context 'when config other_secret is present' do
+      it 'returns request_headers and request_body accordingly' do
         server = create(:server)
+        server_webhook_config = create(
+          :server_webhook_config,
+          server: server,
+          secret: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+          other_secret: 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',
+        )
         server_webhook_event = create(
           :server_webhook_event,
-          server:            server,
-          type:              'test.event_type',
-          last_published_at: '2023-03-20T12:50:01Z',
-          failed_attempts:    1,
-          payload:            { 'server_vote' => { 'id' => 123 } }
+          server: server,
+          config: server_webhook_config,
+          type: 'test.event',
+          last_published_at: '2024-09-02T12:00:01Z',
+          failed_attempts: 1,
+          payload: { 'server_vote' => { 'uuid' => '11111111-1111-1111-1111-111111111111' } }
         )
 
         request_headers, request_body = described_class.new(server_webhook_event).call
 
         expect(request_headers).to eq({
           'Content-Type' => 'application/json',
+          'X-Signature'  => '42026a10921d68149b440c2a5d718e65c2259a0d8acbb6c7087fe993040400a6',
+          'X-Other-Signature' => '68f5efe71532a58da09ba8d699fdf56f08498f429a19742bfd9965a55a38186e',
         })
         expect(request_body).to eq({
-          'event' => {
-            'type'              => 'test.event_type',
-            'last_published_at' => '2023-03-20T12:50:01.000Z',
+          'webhook_event' => {
+            'type'              => 'test.event',
+            'payload'           => { 'server_vote' => { 'uuid' => '11111111-1111-1111-1111-111111111111' } },
+            'last_published_at' => '2024-09-02T12:00:01Z',
             'failed_attempts'   => 1,
-            'payload'           => { 'server_vote' => { 'id' => 123 } },
-          }
-        }.to_json)
-      end
-    end
-
-    context 'when server has ServerWebhookSecrets' do
-      it 'returns request_headers and request_body accordingly' do
-        server = create(:server)
-        create(
-          :server_webhook_secret,
-          server:      server,
-          secret:      'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',
-          archived_at: Time.current
-        )
-        create(
-          :server_webhook_secret,
-          server:      server,
-          secret:      'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-          archived_at: nil
-        )
-        server_webhook_event = create(
-          :server_webhook_event,
-          server:            server,
-          type:              'test.event_type',
-          last_published_at: '2023-03-20T12:50:01Z',
-          failed_attempts:    1,
-          payload:            { 'server_vote' => { 'id' => 123 } }
-        )
-
-        request_headers, request_body = described_class.new(server_webhook_event).call
-
-        expect(request_headers).to eq({
-          'Content-Type'                          => 'application/json',
-          'X-Upper-Town-Server-Webhook-Signature' => 'ee6e31886acf4d0ccfd252db2e39d436730d29b8c0ece6ba4b7f0fcfbc4416dc,509ae33223ef90d03adefa98e22c6728c65f2b31007b1ff2dfe1299e33034201'
-        })
-        expect(request_body).to eq({
-          'event' => {
-            'type'              => 'test.event_type',
-            'last_published_at' => '2023-03-20T12:50:01.000Z',
-            'failed_attempts'   => 1,
-            'payload'           => { 'server_vote' => { 'id' => 123 } },
           }
         }.to_json)
       end
     end
   end
 end
-# rubocop:enable Layout/LineLength
