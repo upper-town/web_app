79076f9445233a9f01a65e8d27f2cb9d4f277289
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2024-09-09 17:41:32 -0300

    Add spec for AdminSession/Session.find_by_token

--- spec/models/admin_session_spec.rb
+++ spec/models/admin_session_spec.rb
@@ -34,6 +34,28 @@ RSpec.describe AdminSession do
     end
   end
 
+  describe '.find_by_token' do
+    context 'when token is blank' do
+      it 'returns nil' do
+        expect(described_class.find_by_token(' ')).to be_nil
+      end
+    end
+
+    context 'when AdminSession is not found' do
+      it 'returns nil' do
+        expect(described_class.find_by_token('abcdef123456')).to be_nil
+      end
+    end
+
+    context 'when AdminSession is found' do
+      it 'returns AdminSession record' do
+        admin_session = create(:admin_session, token_digest: TokenGenerator::AdminSession.digest('abcdef123456'))
+
+        expect(described_class.find_by_token('abcdef123456')).to eq(admin_session)
+      end
+    end
+  end
+
   describe '#expired?' do
     context 'when expires_at is the current time' do
       it 'returns true' do
--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -34,6 +34,28 @@ RSpec.describe Session do
     end
   end
 
+  describe '.find_by_token' do
+    context 'when token is blank' do
+      it 'returns nil' do
+        expect(described_class.find_by_token(' ')).to be_nil
+      end
+    end
+
+    context 'when Session is not found' do
+      it 'returns nil' do
+        expect(described_class.find_by_token('abcdef123456')).to be_nil
+      end
+    end
+
+    context 'when Session is found' do
+      it 'returns Session record' do
+        session = create(:session, token_digest: TokenGenerator::Session.digest('abcdef123456'))
+
+        expect(described_class.find_by_token('abcdef123456')).to eq(session)
+      end
+    end
+  end
+
   describe '#expired?' do
     context 'when expires_at is the current time' do
       it 'returns true' do
