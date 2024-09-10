ab700d8344249cfca929da609560c6a3f38c454e
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2024-09-10 10:29:04 -0300

    Add specs for Pagination

--- app/services/pagination.rb
+++ app/services/pagination.rb
@@ -1,35 +1,33 @@
 # frozen_string_literal: true
 
 class Pagination
-  HARD_MIN = 1
   HARD_MAX = 500
 
   DEFAULT_OPTIONS = {
     page:              1,
-    page_from_request: true,
     page_max:          200,
+    page_from_request: true,
 
     per_page:              20,
-    per_page_from_request: false,
     per_page_max:          100,
+    per_page_from_request: false,
 
     total_count: nil,
   }
 
   attr_reader(
-    :original_relation,
+    :relation,
     :request,
     :options,
     :page,
     :per_page,
-    :offset,
-    :request_helper
+    :offset
   )
 
-  def initialize(relation, request, options: {})
-    @original_relation = relation
+  def initialize(relation, request, **options)
+    @relation = relation
     @request = request
-    @options = DEFAULT_OPTIONS.merge(options)
+    @options = DEFAULT_OPTIONS.merge(options.compact)
 
     @page     = choose_page
     @per_page = choose_per_page
@@ -46,101 +44,103 @@ class Pagination
     @page_size ||= results.size
   end
 
-  def prev_page
-    @prev_page ||= [page - 1, HARD_MIN].max
+  # You can provide total_count via options.
+  # When not provided, it will only be computed when
+  #   you call it
+  #   you call total_pages
+  #   you call last_page
+  #   you call last_page?
+  #   you call last_page_url
+  def total_count
+    @total_count ||= [options[:total_count] || relation.count, 0].max
   end
 
-  def next_page
-    @next_page ||=
-      if page_size < per_page || relation_plus_one.size <= per_page
-        page
-      else
-        [page + 1, options[:page_max], HARD_MAX].min
-      end
+  # total_pages depends on total_count
+  def total_pages
+    @total_pages ||= [
+      (total_count.to_f / per_page).ceil, options[:page_max]
+    ].min.clamp(1, HARD_MAX)
   end
 
-  def prev_page?
-    page > 1
+  # last_page depends on total_count
+  def last_page
+    total_pages
   end
 
-  def next_page?
-    next_page > page
+  # last_page? depends on total_count
+  def last_page?
+    page >= total_pages
   end
 
-  def prev_page_url
-    page_url(prev_page)
-  end
-
-  def next_page_url
-    page_url(next_page)
+  # last_page_url depends on total_count
+  def last_page_url
+    page_url(last_page)
   end
 
   def first_page
     1
   end
 
-  def last_page
-    total_pages.clamp(HARD_MIN, HARD_MAX)
-  end
-
   def first_page?
     page <= 1
-  end
-
-  def last_page?
-    page >= total_pages
   end
 
   def first_page_url
     page_url(first_page)
   end
 
-  def last_page_url
-    page_url(last_page)
+  def prev_page
+    @prev_page ||= [page - 1, 1].max
+  end
+
+  def has_prev_page?
+    page > 1
+  end
+
+  def prev_page_url
+    page_url(prev_page)
+  end
+
+  def next_page
+    @next_page ||= if page_size < per_page || relation_plus_one.size <= per_page
+      page
+    else
+      [page + 1, options[:page_max], HARD_MAX].min
+    end
+  end
+
+  def has_next_page?
+    next_page > page
+  end
+
+  def next_page_url
+    page_url(next_page)
   end
 
   def page_url(value)
     if options[:per_page_from_request]
-      request_helper.url_with_query_params({ 'page' => value, 'per_page' => per_page })
+      @request_helper.url_with_query_params({ 'page' => value, 'per_page' => per_page })
     else
-      request_helper.url_with_query_params({ 'page' => value })
+      @request_helper.url_with_query_params({ 'page' => value })
     end
-  end
-
-  def total_count
-    @total_count ||= options[:total_count] || original_relation.count
-  end
-
-  def total_pages
-    @total_pages ||= [
-      (total_count.to_f / per_page).ceil,
-      options[:page_max],
-      HARD_MAX
-    ].min
   end
 
   private
 
   def choose_page
     if options[:page_from_request]
-      (request.params[:page].presence || options[:page]).to_i
+      request.params['page'].presence || options[:page]
     else
       options[:page]
-    end.clamp(
-      HARD_MIN,
-      [options[:page_max], HARD_MAX].min
-    )
+    end.to_i.clamp(1, [options[:page_max], HARD_MAX].min)
   end
 
   def choose_per_page
     if options[:per_page_from_request]
-      (request.params[:per_page].presence || options[:per_page]).to_i
+      request.params['per_page'].presence || options[:per_page]
     else
       options[:per_page]
-    end.clamp(
-      HARD_MIN,
-      [options[:per_page_max], HARD_MAX].min
-    )
+    end.to_i.clamp(1, [options[:per_page_max], HARD_MAX].min)
   end
 
   def calc_offset
@@ -149,7 +149,7 @@ class Pagination
 
   def relation_plus_one
     @relation_plus_one ||= begin
-      rel = original_relation.offset(offset).limit(per_page + 1)
+      rel = relation.offset(offset).limit(per_page + 1)
       rel.load
       rel
     end
---
+++ spec/services/pagination_spec.rb
@@ -0,0 +1,392 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Pagination do
+  describe '#page' do
+    it 'gets page from options, clamps value' do
+      relation = User.order(id: :asc)
+      [
+        [20,   nil, false, nil, 20],
+        ['20', nil, false, nil, 20],
+        [20,   nil, false, 30,  20],
+        [20,   10,  false, nil, 10],
+        [20,   nil, true, nil,  20],
+        [20,   nil, true, 25,   25],
+        [20,   nil, true, '25', 25],
+        [20,   10,  true, 25,   10],
+
+        [ 1,   nil, false, nil, 1],
+        [ '1', nil, false, nil, 1],
+        [ 1,   nil, false, 5,   1],
+        [ 1,   nil, true,  nil, 1],
+        [ 1,   nil, true,  5,   5],
+        [-1,   nil, false, nil, 1],
+        ['-1', nil, false, nil, 1],
+        [-1,   nil, false, 5,   1],
+        [-1,   nil, true,  nil, 1],
+        [-1,   nil, true,  5,   5],
+        [-1,   nil, true,  '5', 5],
+
+        [501, nil, false, nil,   200],
+        [501, nil, false, 300,   200],
+        [501, 300, false, nil,   300],
+        [501, 300, true,  nil,   300],
+        [501, nil, true,  300,   200],
+        [501, 300, true,  300,   300],
+        [501, 300, true,  '300', 300],
+
+        [501, 1_000, false, nil,   500],
+        [501, 1_000, false, 300,   500],
+        [501, 1_000, true,  nil,   500],
+        [501, 1_000, true,  300,   300],
+        [501, 1_000, true,  '300', 300],
+      ].each do |page, page_max, page_from_request, request_page_param, expected_page|
+        request = TestRequestHelper.build(params: { 'page' => request_page_param })
+        pagination = described_class.new(
+          relation,
+          request,
+          page: page,
+          page_max: page_max,
+          page_from_request: page_from_request
+        )
+
+        expect(pagination.page).to eq(expected_page), "Failed for #{page.inspect} and #{expected_page.inspect}"
+      end
+    end
+  end
+
+  describe '#per_page' do
+    it 'gets per_page from options, clamps value' do
+      relation = User.order(id: :asc)
+      [
+        [20,   nil, false, nil, 20],
+        ['20', nil, false, nil, 20],
+        [20,   nil, false, 30,  20],
+        [20,   10,  false, nil, 10],
+        [20,   nil, true, nil,  20],
+        [20,   nil, true, 25,   25],
+        [20,   nil, true, '25', 25],
+        [20,   10,  true, 25,   10],
+
+        [ 1,   nil, false, nil, 1],
+        [ '1', nil, false, nil, 1],
+        [ 1,   nil, false, 5,   1],
+        [ 1,   nil, true,  nil, 1],
+        [ 1,   nil, true,  5,   5],
+        [-1,   nil, false, nil, 1],
+        ['-1', nil, false, nil, 1],
+        [-1,   nil, false, 5,   1],
+        [-1,   nil, true,  nil, 1],
+        [-1,   nil, true,  5,   5],
+        [-1,   nil, true,  '5', 5],
+
+        [501, nil, false, nil,   100],
+        [501, nil, false, 300,   100],
+        [501, 300, false, nil,   300],
+        [501, 300, true,  nil,   300],
+        [501, nil, true,  300,   100],
+        [501, 300, true,  300,   300],
+        [501, 300, true,  '300', 300],
+
+        [501, 1_000, false, nil,   500],
+        [501, 1_000, false, 300,   500],
+        [501, 1_000, true,  nil,   500],
+        [501, 1_000, true,  300,   300],
+        [501, 1_000, true,  '300', 300],
+      ].each do |per_page, per_page_max, per_page_from_request, request_per_page_param, expected_per_page|
+        request = TestRequestHelper.build(params: { 'per_page' => request_per_page_param })
+        pagination = described_class.new(
+          relation,
+          request,
+          per_page: per_page,
+          per_page_max: per_page_max,
+          per_page_from_request: per_page_from_request
+        )
+
+        expect(pagination.per_page).to eq(expected_per_page), "Failed for #{per_page.inspect} and #{expected_per_page.inspect}"
+      end
+    end
+  end
+
+  describe '#offset' do
+    it 'calculates offset according to per_page and page' do
+      relation = User.order(id: :asc)
+      request = TestRequestHelper.build
+      [
+        [20, 1,  0],
+        [20, 2, 20],
+        [20, 3, 40],
+        [20, 4, 60],
+      ].each do |per_page, page, expected_offset|
+        pagination = described_class.new(
+          relation,
+          request,
+          per_page: per_page,
+          page: page
+        )
+
+        expect(pagination.offset).to eq(expected_offset)
+      end
+    end
+  end
+
+  describe '#results and #page_size' do
+    describe 'order asc' do
+      it 'takes per_page items from relation with offset for page' do
+        users = create_list(:user, 10)
+        relation = User.order(id: :asc)
+        request = TestRequestHelper.build
+
+        pagination = described_class.new(relation, request, per_page: 3, page: 1)
+        expect(pagination.results).to eq([users[0], users[1], users[2]])
+        expect(pagination.page_size).to eq(3)
+
+        pagination = described_class.new(relation, request, per_page: 3, page: 2)
+        expect(pagination.results).to eq([users[3], users[4], users[5]])
+        expect(pagination.page_size).to eq(3)
+
+        pagination = described_class.new(relation, request, per_page: 3, page: 3)
+        expect(pagination.results).to eq([users[6], users[7], users[8]])
+        expect(pagination.page_size).to eq(3)
+
+        pagination = described_class.new(relation, request, per_page: 3, page: 4)
+        expect(pagination.results).to eq([users[9]])
+        expect(pagination.page_size).to eq(1)
+
+        pagination = described_class.new(relation, request, per_page: 3, page: 5)
+        expect(pagination.results).to be_empty
+        expect(pagination.page_size).to eq(0)
+      end
+    end
+
+    describe 'order desc' do
+      it 'takes per_page items from relation with offset for page' do
+        users = create_list(:user, 10)
+        relation = User.order(id: :desc)
+        request = TestRequestHelper.build
+
+        pagination = described_class.new(relation, request, per_page: 3, page: 1)
+        expect(pagination.results).to eq([users[9], users[8], users[7]])
+        expect(pagination.page_size).to eq(3)
+
+        pagination = described_class.new(relation, request, per_page: 3, page: 2)
+        expect(pagination.results).to eq([users[6], users[5], users[4]])
+        expect(pagination.page_size).to eq(3)
+
+        pagination = described_class.new(relation, request, per_page: 3, page: 3)
+        expect(pagination.results).to eq([users[3], users[2], users[1]])
+        expect(pagination.page_size).to eq(3)
+
+        pagination = described_class.new(relation, request, per_page: 3, page: 4)
+        expect(pagination.results).to eq([users[0]])
+        expect(pagination.page_size).to eq(1)
+
+        pagination = described_class.new(relation, request, per_page: 3, page: 5)
+        expect(pagination.results).to be_empty
+        expect(pagination.page_size).to eq(0)
+      end
+    end
+  end
+
+  describe '#total_count, #total_pages, #last_page, #last_page?' do
+    context 'when it is zero' do
+      it 'returns accordingly' do
+        create_list(:user, 10)
+        relation = User.order(id: :asc)
+        request = TestRequestHelper.build
+
+        pagination = described_class.new(relation, request, total_count: 0, per_page: 3)
+
+        expect(pagination.total_count).to eq(0)
+        expect(pagination.total_pages).to eq(1)
+        expect(pagination.last_page).to eq(1)
+        expect(pagination.last_page?).to be(true)
+      end
+    end
+
+    context 'when total_count option is given' do
+      it 'returns it' do
+        create_list(:user, 10)
+        relation = User.order(id: :asc)
+        request = TestRequestHelper.build
+
+        pagination = described_class.new(relation, request, total_count: 100, per_page: 3)
+
+        expect(pagination.total_count).to eq(100)
+        expect(pagination.total_pages).to eq(34)
+        expect(pagination.last_page).to eq(34)
+        expect(pagination.last_page?).to be(false)
+
+        pagination = described_class.new(relation, request, total_count: 100, per_page: 3, page: 34)
+
+        expect(pagination.total_count).to eq(100)
+        expect(pagination.total_pages).to eq(34)
+        expect(pagination.last_page).to eq(34)
+        expect(pagination.last_page?).to be(true)
+      end
+    end
+
+    context 'when total_count option is not given' do
+      it 'returns relation count' do
+        create_list(:user, 10)
+        relation = User.order(id: :asc)
+        request = TestRequestHelper.build
+
+        pagination = described_class.new(relation, request, total_count: nil, per_page: 3, page: 1)
+
+        expect(pagination.total_count).to eq(10)
+        expect(pagination.total_pages).to eq(4)
+        expect(pagination.last_page).to eq(4)
+        expect(pagination.last_page?).to be(false)
+
+        pagination = described_class.new(relation, request, total_count: nil, per_page: 3, page: 4)
+
+        expect(pagination.total_count).to eq(10)
+        expect(pagination.total_pages).to eq(4)
+        expect(pagination.last_page).to eq(4)
+        expect(pagination.last_page?).to be(true)
+      end
+    end
+  end
+
+  describe '#first_page and #first_page?' do
+    it 'returns accordingly' do
+      create_list(:user, 10)
+      relation = User.order(id: :asc)
+      request = TestRequestHelper.build
+
+      pagination = described_class.new(relation, request, per_page: 4, page: 1)
+      expect(pagination.first_page).to eq(1)
+      expect(pagination.first_page?).to be(true)
+
+      pagination = described_class.new(relation, request, per_page: 4, page: 2)
+      expect(pagination.first_page).to eq(1)
+      expect(pagination.first_page?).to be(false)
+    end
+  end
+
+  describe '#prev_page and #has_prev_page?' do
+    it 'returns accordingly' do
+      [
+        [-1, 1, false],
+        [ 0, 1, false],
+        [ 1, 1, false],
+        [ 2, 1, true],
+        [ 3, 2, true],
+        [ 4, 3, true],
+        [10, 9, true],
+      ].each do |page, expected_prev_page, expected_has_prev_page|
+        relation = User.order(id: :asc)
+        request = TestRequestHelper.build
+        pagination = described_class.new(relation, request, page: page)
+
+        expect(pagination.prev_page).to eq(expected_prev_page)
+        expect(pagination.has_prev_page?).to be(expected_has_prev_page)
+      end
+    end
+  end
+
+  describe '#next_page and #has_next_page?' do
+    context 'when page_size is less than per_page' do
+      it 'returns page' do
+        relation = User.order(id: :asc)
+        request = TestRequestHelper.build
+        pagination = described_class.new(relation, request, per_page: 10, page: 1)
+
+        expect(pagination.next_page).to eq(1)
+        expect(pagination.has_next_page?).to be(false)
+      end
+    end
+
+    context 'when relation_plus_one.size is less than per_page' do
+      it 'returns page' do
+        create_list(:user, 4)
+        relation = User.order(id: :asc)
+        request = TestRequestHelper.build
+        pagination = described_class.new(relation, request, per_page: 5, page: 1)
+
+        expect(pagination.next_page).to eq(1)
+        expect(pagination.has_next_page?).to be(false)
+      end
+    end
+
+    context 'when relation_plus_one.size is equal to per_page' do
+      it 'returns page' do
+        create_list(:user, 5)
+        relation = User.order(id: :asc)
+        request = TestRequestHelper.build
+        pagination = described_class.new(relation, request, per_page: 5, page: 1)
+
+        expect(pagination.next_page).to eq(1)
+        expect(pagination.has_next_page?).to be(false)
+      end
+    end
+
+    context 'when relation_plus_one.size is greater than per_page' do
+      it 'returns page + 1 or respects page_max' do
+        create_list(:user, 6)
+        relation = User.order(id: :asc)
+        request = TestRequestHelper.build
+
+        pagination = described_class.new(relation, request, per_page: 5, page: 1)
+        expect(pagination.next_page).to eq(2)
+        expect(pagination.has_next_page?).to be(true)
+
+        pagination = described_class.new(relation, request, per_page: 5, page: 1, page_max: 1)
+        expect(pagination.next_page).to eq(1)
+        expect(pagination.has_next_page?).to be(false)
+      end
+    end
+  end
+
+  describe '#first_page_url, #prev_page_url, #page_url, #next_page_url, #last_page_url' do
+    it 'returns accordingly' do
+      create_list(:user, 10)
+      relation = User.order(id: :asc)
+      request = TestRequestHelper.build(url: 'http://test.upper.town/servers')
+
+      pagination = described_class.new(relation, request, per_page: 4, page: 1)
+      expect(pagination.first_page_url).to eq('http://test.upper.town/servers?page=1')
+      expect(pagination.prev_page_url).to eq('http://test.upper.town/servers?page=1')
+      expect(pagination.page_url(1)).to eq('http://test.upper.town/servers?page=1')
+      expect(pagination.next_page_url).to eq('http://test.upper.town/servers?page=2')
+      expect(pagination.last_page_url).to eq('http://test.upper.town/servers?page=3')
+
+      pagination = described_class.new(relation, request, per_page: 4, page: 1, per_page_from_request: true)
+      expect(pagination.first_page_url).to eq('http://test.upper.town/servers?page=1&per_page=4')
+      expect(pagination.prev_page_url).to eq('http://test.upper.town/servers?page=1&per_page=4')
+      expect(pagination.page_url(1)).to eq('http://test.upper.town/servers?page=1&per_page=4')
+      expect(pagination.next_page_url).to eq('http://test.upper.town/servers?page=2&per_page=4')
+      expect(pagination.last_page_url).to eq('http://test.upper.town/servers?page=3&per_page=4')
+
+      pagination = described_class.new(relation, request, per_page: 4, page: 2)
+      expect(pagination.first_page_url).to eq('http://test.upper.town/servers?page=1')
+      expect(pagination.prev_page_url).to eq('http://test.upper.town/servers?page=1')
+      expect(pagination.page_url(2)).to eq('http://test.upper.town/servers?page=2')
+      expect(pagination.next_page_url).to eq('http://test.upper.town/servers?page=3')
+      expect(pagination.last_page_url).to eq('http://test.upper.town/servers?page=3')
+
+      pagination = described_class.new(relation, request, per_page: 4, page: 2, per_page_from_request: true)
+      expect(pagination.first_page_url).to eq('http://test.upper.town/servers?page=1&per_page=4')
+      expect(pagination.prev_page_url).to eq('http://test.upper.town/servers?page=1&per_page=4')
+      expect(pagination.page_url(2)).to eq('http://test.upper.town/servers?page=2&per_page=4')
+      expect(pagination.next_page_url).to eq('http://test.upper.town/servers?page=3&per_page=4')
+      expect(pagination.last_page_url).to eq('http://test.upper.town/servers?page=3&per_page=4')
+
+      pagination = described_class.new(relation, request, per_page: 4, page: 3)
+      expect(pagination.first_page_url).to eq('http://test.upper.town/servers?page=1')
+      expect(pagination.prev_page_url).to eq('http://test.upper.town/servers?page=2')
+      expect(pagination.page_url(3)).to eq('http://test.upper.town/servers?page=3')
+      expect(pagination.next_page_url).to eq('http://test.upper.town/servers?page=3')
+      expect(pagination.last_page_url).to eq('http://test.upper.town/servers?page=3')
+
+      pagination = described_class.new(relation, request, per_page: 4, page: 3, per_page_from_request: true)
+      expect(pagination.first_page_url).to eq('http://test.upper.town/servers?page=1&per_page=4')
+      expect(pagination.prev_page_url).to eq('http://test.upper.town/servers?page=2&per_page=4')
+      expect(pagination.page_url(3)).to eq('http://test.upper.town/servers?page=3&per_page=4')
+      expect(pagination.next_page_url).to eq('http://test.upper.town/servers?page=3&per_page=4')
+      expect(pagination.last_page_url).to eq('http://test.upper.town/servers?page=3&per_page=4')
+    end
+  end
+end
