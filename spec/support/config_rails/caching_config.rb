7692953cdff096ea48513fb5541010e2c9bdc9d2
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2025-01-08 14:08:19 -0300

    Move Caching and RateLimiting config to config_rails

--- spec/support/config_rails/caching_config.rb
+++ spec/support/config_rails/caching_config.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative '../../../app/concepts/caching'
+
 RSpec.configure do |config|
   config.before do
     Caching.redis.flushdb
--- spec/support/config_rails/rate_limiting_config.rb
+++ spec/support/config_rails/rate_limiting_config.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative '../../../app/concepts/rate_limiting'
+
 RSpec.configure do |config|
   config.before do
     RateLimiting.redis.flushdb
