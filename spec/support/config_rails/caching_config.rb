33d77d19bc563b9d76110a334feb9a0b7ef3aff6
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2025-01-08 14:59:12 -0300

    Remove unnecessary require in config_rails files

--- spec/support/config_rails/caching_config.rb
+++ spec/support/config_rails/caching_config.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative '../../../app/concepts/caching'
-
 RSpec.configure do |config|
   config.before do
     Caching.redis.flushdb
--- spec/support/config_rails/rate_limiting_config.rb
+++ spec/support/config_rails/rate_limiting_config.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative '../../../app/concepts/rate_limiting'
-
 RSpec.configure do |config|
   config.before do
     RateLimiting.redis.flushdb
