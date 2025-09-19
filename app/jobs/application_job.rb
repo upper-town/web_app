926a4402d800b5461e377a28e6d7083fd5974913
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2025-09-19 09:45:27 -0300

    Retry jobs with :polynomially_longer wait times

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -4,5 +4,5 @@ class ApplicationJob < ActiveJob::Base
   queue_as "default"
 
   # Automatically retry jobs on error
-  retry_on StandardError
+  retry_on StandardError, wait: :polynomially_longer, attempts: 25
 end
